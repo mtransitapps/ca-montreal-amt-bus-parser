9ba971eb196dd391788e372a75412806aea01cbf
Mathieu Méa <mathieu.mea@gmail.com>
2014-10-06 14:59:59 -0400

    Initial commit

---
+++ src/org/mtransit/parser/ca_montreal_amt_bus/MontrealAMTBusAgencyTools.java
@@ -0,0 +1,73 @@
+package org.mtransit.parser.ca_montreal_amt_bus;
+
+import org.mtransit.parser.DefaultAgencyTools;
+import org.mtransit.parser.Utils;
+import org.mtransit.parser.gtfs.data.GRoute;
+import org.mtransit.parser.gtfs.data.GStop;
+import org.mtransit.parser.gtfs.data.GTrip;
+import org.mtransit.parser.mt.data.MTrip;
+
+public class MontrealAMTBusAgencyTools extends DefaultAgencyTools {
+
+	public static final String ROUTE_TYPE_FILTER = "3"; // bus only
+
+	public static void main(String[] args) {
+		if (args == null || args.length == 0) {
+			args = new String[3];
+			args[0] = "input/gtfs.zip";
+			args[1] = "../ca-montreal-amt-bus/res/raw/";
+			args[2] = ""; // files-prefix
+		}
+		new MontrealAMTBusAgencyTools().start(args);
+	}
+
+	@Override
+	public void start(String[] args) {
+		System.out.printf("Generating AMT bus data...\n");
+		long start = System.currentTimeMillis();
+		super.start(args);
+		System.out.printf("Generating AMT bus data... DONE in %s.\n", Utils.getPrettyDuration(System.currentTimeMillis() - start));
+	}
+
+	@Override
+	public boolean excludeRoute(GRoute gRoute) {
+		if (ROUTE_TYPE_FILTER != null && !gRoute.route_type.equals(ROUTE_TYPE_FILTER)) {
+			return true;
+		}
+		return super.excludeRoute(gRoute);
+	}
+
+	@Override
+	public void setTripHeadsign(MTrip mTrip, GTrip gTrip) {
+		String stationName = cleanTripHeadsign(gTrip.trip_headsign);
+		int directionId = Integer.valueOf(gTrip.direction_id);
+		mTrip.setHeadsignString(stationName, directionId);
+	}
+
+	private static final String DIRECTION = "Direction ";
+
+	private String cleanTripHeadsign(String gTripHeading) {
+		return cleanStopName(gTripHeading.substring(DIRECTION.length()));
+	}
+
+	public static final String PLACE_CHAR_DE = "de ";
+	public static final String PLACE_CHAR_RUE = "rue ";
+
+	@Override
+	public String cleanStopName(String gStopName) {
+		String result = gStopName;
+		if (result.contains(PLACE_CHAR_DE)) {
+			result = result.replace(PLACE_CHAR_DE, "");
+		}
+		if (result.contains(PLACE_CHAR_RUE)) {
+			result = result.replace(PLACE_CHAR_RUE, "");
+		}
+		return super.cleanStopName(result);
+	}
+
+	@Override
+	public int getStopId(GStop gStop) {
+		return Integer.valueOf(getStopCode(gStop)); // using stop code as stop ID
+	}
+
+}
