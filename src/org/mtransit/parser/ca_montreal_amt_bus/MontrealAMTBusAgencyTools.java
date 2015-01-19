05b713658dfbd6b2aff5e696b86ba12ddb4f3097
Mathieu Méa <mathieu.mea@gmail.com>
2015-01-19 09:59:48 -0500

    Filter on service IDs.

--- src/org/mtransit/parser/ca_montreal_amt_bus/MontrealAMTBusAgencyTools.java
+++ src/org/mtransit/parser/ca_montreal_amt_bus/MontrealAMTBusAgencyTools.java
@@ -1,7 +1,11 @@
 package org.mtransit.parser.ca_montreal_amt_bus;
 
+import java.util.HashSet;
+
 import org.mtransit.parser.DefaultAgencyTools;
 import org.mtransit.parser.Utils;
+import org.mtransit.parser.gtfs.data.GCalendar;
+import org.mtransit.parser.gtfs.data.GCalendarDate;
 import org.mtransit.parser.gtfs.data.GRoute;
 import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.gtfs.data.GTrip;
@@ -25,12 +29,39 @@ public class MontrealAMTBusAgencyTools extends DefaultAgencyTools {
 		new MontrealAMTBusAgencyTools().start(args);
 	}
 
+	private HashSet<String> serviceIds;
+
 	@Override
 	public void start(String[] args) {
 		System.out.printf("Generating AMT bus data...\n");
 		long start = System.currentTimeMillis();
+		this.serviceIds = extractUsefulServiceIds(args, this);
 		super.start(args);
 		System.out.printf("Generating AMT bus data... DONE in %s.\n", Utils.getPrettyDuration(System.currentTimeMillis() - start));
+	}
+
+	@Override
+	public boolean excludeCalendar(GCalendar gCalendar) {
+		if (this.serviceIds != null) {
+			return excludeUselessCalendar(gCalendar, this.serviceIds);
+		}
+		return super.excludeCalendar(gCalendar);
+	}
+
+	@Override
+	public boolean excludeCalendarDate(GCalendarDate gCalendarDates) {
+		if (this.serviceIds != null) {
+			return excludeUselessCalendarDate(gCalendarDates, this.serviceIds);
+		}
+		return super.excludeCalendarDate(gCalendarDates);
+	}
+
+	@Override
+	public boolean excludeTrip(GTrip gTrip) {
+		if (this.serviceIds != null) {
+			return excludeUselessTrip(gTrip, serviceIds);
+		}
+		return super.excludeTrip(gTrip);
 	}
 
 	@Override
